f8ec9df58386b88cfd27645ea0d1f54c84ed5711
[dyad] <brunofranchi9@gmail.com>
2026-01-15 20:54:32 +0100

    [dyad] Ottimizzato il componente Footer per la visualizzazione mobile riducendo le dimensioni dei loghi e lo spazio tra di essi per eliminare lo scorrimento orizzontale. - wrote 1 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,17 +16,17 @@ const Footer = () => {
     <footer className="bg-gray-50 border-t border-gray-200 mt-12 pt-8 pb-4 dark:bg-gray-900 dark:border-gray-800">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         
-        {/* Institutional Logos */}
-        <div className="flex justify-center items-center space-x-8 mb-8">
+        {/* Institutional Logos - Optimized for mobile */}
+        <div className="flex justify-center items-center space-x-4 sm:space-x-8 mb-8">
           <img 
             src="/assets/coni.jpeg" 
             alt="Logo CONI" 
-            className="h-16 w-auto object-contain opacity-80 dark:opacity-90"
+            className="h-12 sm:h-16 w-auto object-contain opacity-80 dark:opacity-90"
           />
           <img 
             src="/assets/fitp.jpeg" 
             alt="Logo FITP" 
-            className="h-16 w-auto object-contain opacity-80 dark:opacity-90"
+            className="h-12 sm:h-16 w-auto object-contain opacity-80 dark:opacity-90"
           />
         </div>
 
