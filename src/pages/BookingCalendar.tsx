c8b6a20bbf115141b1e678862337c584b12aae93
[dyad] <brunofranchi9@gmail.com>
2026-02-16 21:21:03 +0100

    Reverted all changes back to version 52c7fac8d974c9a3ae2d4696bbfa8d6d1a9f6113

--- src/pages/BookingCalendar.tsx
+++ src/pages/BookingCalendar.tsx
@@ -243,7 +243,7 @@ const BookingCalendar = () => {
 
       const finalReservations = (inserted && inserted.length > 0) ? inserted : (reservationsToInsert as any);
 
-      showSuccess("Prenotazione effettuata!");
+      showSuccess("Prenotazione confermata!");
       navigate('/booking-confirmation', { 
         state: { 
           reservations: finalReservations, 
