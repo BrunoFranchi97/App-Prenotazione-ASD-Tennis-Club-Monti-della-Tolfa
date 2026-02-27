5d7131867f3378638479f5a9bed6aa7dc392f0cf
[dyad] <brunofranchi9@gmail.com>
2026-02-27 19:02:19 +0100

    [dyad] Risolto errore Label non definita in gestione soci - wrote 1 file(s)

--- src/pages/AdminUserManagement.tsx
+++ src/pages/AdminUserManagement.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Badge } from "@/components/ui/badge";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
